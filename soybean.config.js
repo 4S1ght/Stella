d536c5eedf82ce15637afd371b2a6544d1ba60e7
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-11-09 01:42:38 +0100

    [add] Added... A lot...

--- soybean.config.js
+++ soybean.config.js
@@ -6,14 +6,17 @@ import https from 'https'
 
 // ==================================================================
 
-const types = [
+const docsPage = 'https://code.visualstudio.com/api/references/theme-color'
+let docsKeys
+let lintScope = ""
+
+const schema = new yaml.DEFAULT_SCHEMA.extend([
     new yaml.Type('!a', {
         kind: 'sequence',
         construct: ([hexRGB, alpha]) => hexRGB + alpha,
         represent: ([hexRGB, alpha]) => hexRGB + alpha,
     })
-]
-const schema = new yaml.DEFAULT_SCHEMA.extend(types)
+])
 
 const getDocsKeys = async e => {
 
@@ -25,7 +28,7 @@ const getDocsKeys = async e => {
      */
     const blacklistedKeys = [ 'workbench.colorCustomizations', 'editor.tokenColorCustomizations', ]
     const response = await (new Promise((resolve, reject) => {
-        https.get('https://code.visualstudio.com/api/references/theme-color', res => {
+        https.get(docsPage, res => {
             let body = ''
             res.setEncoding('utf8')
             res.on('data', data => (body += data))
@@ -34,22 +37,20 @@ const getDocsKeys = async e => {
         })
     }))
 
-    const matches = response.match(new RegExp('<code>.+?</code>', 'g'))
+    const matches = response.match(new RegExp('<code>.+?</code>.+?</li>', 'g'))
 
     docsKeys = [...matches]
-        .map(key => key.replace('<code>', '').replace('</code>', ''))
-        .filter(key => !/ /.test(key))                 // Remove if contains spaces
-        .filter(key => !/#.../.test(key))              // Remove if is a hex color
-        .filter(key => !/&quot;/.test(key))            // Remove if contains quotes
-        .filter(key => key.length > 4)                 // Remove if it's very small
-        .filter(key => !blacklistedKeys.includes(key)) // Remove if its in the blacklist
-        .sort()     
-        
+        .map(key => key.replace(/<code>|<\/code>|<\/li>/g, ''))
+        .filter(key => key.includes(': '))             // Remove if doesn't contain ": "
+        .map(key => key.split(': '))
+        .filter(([key, desc]) => !/ /.test(key))                 // Remove if contains spaces
+        .filter(([key, desc]) => !/#.../.test(key))              // Remove if is a hex color
+        .filter(([key, desc]) => !/&quot;/.test(key))            // Remove if contains quotes
+        .filter(([key, desc]) => key.length > 4)                 // Remove if it's very small
+        .filter(([key, desc]) => !blacklistedKeys.includes(key)) // Remove if its in the blacklist  
+
     return docsKeys
 }
-
-let docsKeys
-let lintScope = ""
 
 // ==================================================================
 
@@ -81,24 +82,27 @@ export default Soybean({
                         e.set('docsKeys', await getDocsKeys())
                     }),
                     h.forIn(Symbol('yamlFile'), h.handle(e => {
-                        const key = e.get('key')
-                        if (!docsKeys.includes(key) && key.indexOf(lintScope) === 0) console.warn(`Unsupported key \x1b[31m${key}\x1b[0m`)
+                        if (!docsKeys.find(([key]) => key === e.key) && e.key.indexOf(lintScope) === 0) {
+                            console.warn(`\x1b[31m${e.key}\x1b[0m`)
+                        }
                     })),
                     h.forEach(Symbol('docsKeys'), h.handle(e => {
-                        const value = e.get('value')
-                        const theme = e.get('yamlFile')
-                        if (!Object.keys(theme.colors || []).includes(value) && value.indexOf(lintScope) === 0) console.warn(`Missing key \x1b[34m${value}\x1b[0m`) 
+                        const [value, desc] = e.value
+                        const theme = e.yamlFile
+                        if (!Object.keys(theme.colors || []).find(x => x === value) && value.indexOf(lintScope) === 0) {
+                            console.warn(`\x1b[34m${value}\x1b[0m: \x1b[30m${desc}\x1b[0m`)
+                        }
                     }))
-
                 ])
             }
         ]
     },
     terminal: {
-        passthroughShell: false,
+        passthroughShell: true,
         keepHistory: 50,
         handlers: {
-            scope: h.handle(e => { lintScope = e.argv[0] || "" })
+            scope: h.handle(e => { lintScope = e.argv[0] || "" }),
+            docs: h.shell.spawn(['open', docsPage])
         }
     }
 })
